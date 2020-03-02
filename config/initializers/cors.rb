766435cc5856783db6c7e5d23c48123909446b7d
Rap Mendoza <mendozarap@gmail.com>
2020-03-03 00:41:35 +0800

    Modified cors to accept heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -10,7 +10,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     end
 
     allow do
-        origins "https://rap-authentication-app.herokuapp.com"
+        origins "https://rails-authentication-app.herokuapp.com"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head],
         credentials: true
     end
